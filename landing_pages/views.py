b1b11250bee3fabf777406645f8b2b4ca2ff0014
Amy Mok <amy.mok@cfpb.gov>
2015-12-01 13:35:13 -0500

    Add base template and change template view to specific class view

--- landing_pages/views.py
+++ landing_pages/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+#BASE_TEMPLATE = "base.html"
+BASE_TEMPLATE = "base_update.html"
+
+class LandingView(TemplateView):
+    template_name = "landing-page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(LandingView, self).get_context_data(**kwargs)
+        context['base_template'] = BASE_TEMPLATE
+        return context
+
+class DataUseView(TemplateView):
+    template_name = "data-use-content.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DataUseView, self).get_context_data(**kwargs)
+        context['base_template'] = BASE_TEMPLATE
+        return context
