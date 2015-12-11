56cc14373819de160d7da94ac992e2e9c6943511
Amy Mok <amy.mok@cfpb.gov>
2015-12-11 16:40:38 -0500

    Add setup.py file for packaging app

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+setup(
+    name='ccdb-content',
+    version='1.0.0',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    maintainer='cfpb',
+    maintainer_email='tech@cfpb.gov',
+    packages=['landing_pages'],
+    include_package_data=True,
+    description=u'CCDB Landing Pages',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.md'),
+    zip_safe=False,
+)
