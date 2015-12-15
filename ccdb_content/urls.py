668de35e91faec7867bb45cf529f77919a81eebf
Amy Mok <amy.mok@cfpb.gov>
2015-12-15 17:17:10 -0500

    Change urls to match our existing urls

--- ccdb_content/urls.py
+++ ccdb_content/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from ccdb_content.views import DataUseView, LandingView, DocsView
 
 urlpatterns = [
-    url(r'^data-use-content/', DataUseView.as_view()),
-    url(r'^landing-page/', LandingView.as_view()),
+    url(r'^$', LandingView.as_view()),
+    url(r'^data-use/', DataUseView.as_view()),
     url(r'^technical-documentation/', DocsView.as_view()),
 ]
