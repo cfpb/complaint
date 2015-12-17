0420614440a4a0102ac3e01535afa3377aa66fef
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-12-17 17:33:34 -0500

    fix url pattern

--- ccdb/urls.py
+++ ccdb/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ccdb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^complaintdatabase/', include('complaintdatabase.urls')),
-    url(r'^complaint/process/', ProcessView.as_view()),
+    url(r'^complaint/process', ProcessView.as_view()),
     url(r'^complaint/', SubmitView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
