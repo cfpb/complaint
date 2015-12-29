15d914d9e0b468ca87ac62e9093d40ea2f3be970
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-12-29 12:00:04 -0500

    rename app_common to complaint_common

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='tech@cfpb.gov',
     maintainer='cfpb',
     maintainer_email='tech@cfpb.gov',
-    packages=['app_common','complaint','complaintdatabase'],
+    packages=['complaint_common','complaint','complaintdatabase'],
     include_package_data=True,
     description=u'CCDB Landing Pages',
     classifiers=[
