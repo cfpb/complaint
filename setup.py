f72c180c1324ff8b93dceb17ebad2710daedf42f
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-02-22 18:28:09 -0500

    update version to 1.1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_file(filename):
 
 setup(
     name='ccdb-content',
-    version='1.1.5',
+    version='1.1.6',
     author='CFPB',
     author_email='tech@cfpb.gov',
     maintainer='cfpb',
